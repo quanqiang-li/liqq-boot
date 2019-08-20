d6a4e56b2fc6f7f43a806217d6f44a09844c3fe5
aisino-liqq <liquanqiang@aisino.com>
2019-08-20 21:21:11 +0800

    集成mybatis

--- src/main/java/com/liqq/Application.java
+++ src/main/java/com/liqq/Application.java
@@ -1,9 +1,11 @@
 package com.liqq;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.liqq.dao.mysql")
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -1,3 +1,17 @@
+/*系统字典表*/
+CREATE TABLE `sys_dict` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `code` varchar(50) DEFAULT NULL COMMENT '代码,如表名,业务分类',
+  `name` varchar(50) DEFAULT NULL COMMENT '名称,对应code',
+  `key` varchar(50) DEFAULT NULL COMMENT '键',
+  `value` varchar(50) DEFAULT NULL COMMENT '值',
+  `sort` int(2) DEFAULT NULL COMMENT '排序',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='系统字典表';
+
+
 /*系统用户*/
 CREATE TABLE `sys_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
@@ -27,7 +41,41 @@ CREATE TABLE `sys_role` (
   `code` varchar(20) DEFAULT NULL COMMENT '代码',
   `name` varchar(50) DEFAULT NULL COMMENT '名称',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`),
   UNIQUE KEY `idx_role_code` (`code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='系统角色';
 
+/*系统用户角色关联表*/
+CREATE TABLE `sys_user_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `user_id` int(11) DEFAULT NULL COMMENT '用户id',
+  `role_id` int(11) DEFAULT NULL COMMENT '角色id',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='系统用户角色关联表';
+
+/*系统资源表*/
+CREATE TABLE `sys_resource` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `parent_id` int(11) NOT NULL COMMENT '父级id',
+  `code` varchar(50) DEFAULT NULL COMMENT '资源代码',
+  `name` varchar(50) DEFAULT NULL COMMENT '资源名称',
+  `href` varchar(255) DEFAULT NULL COMMENT '资源路径',
+  `sort` int(4) DEFAULT NULL COMMENT '咨询排序',
+  `type` tinyint(1) DEFAULT NULL COMMENT '资源类型 0菜单 1功能',
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='系统资源表';
+
+/*系统角色资源关联表*/
+CREATE TABLE `sys_role_resource` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `role_id` int(11) DEFAULT NULL COMMENT '角色id',
+  `resource_id` int(11) DEFAULT NULL COMMENT '资源id',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='系统角色资源关联表';
